c8011a53f7417fa5c66ad6c3e1da7abbc84e5f93
Anshu-pandey-codes <anshu191001@gmail.com>
2021-06-19 00:39:19 +0530

    Added Connect and Disconnect

--- server/server.js
+++ server/server.js
@@ -1,13 +1,37 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
 const port = process.env.PORT || 3000
 
 const app = express();
 const staticpath = path.join(__dirname,'../public');
 
+const createserver = http.createServer(app);
+const io = socketIO(createserver);          // Gives access to Socketio library
+
 app.use(express.static(staticpath));
 
-app.listen(port,()=>{
+io.on('connection',(socket)=>{
+    console.log("A New User JustConnected")
+
+    
+  socket.on('disonnect',(socket)=>{
+    console.log("User Disconnected")
+  }) 
+
+})
+
+
+// ----------------------------------------------------------------
+
+
+// app.listen(port,()=>{
+//     console.log('Connected at ' + port);
+// })
+
+createserver.listen(port,()=>{
     console.log('Connected at ' + port);
 })
+// important to create your own server for Socketio
